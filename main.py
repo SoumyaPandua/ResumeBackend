a65173908cac51caf25c9ba9a5e0aef8bce0cc1a
Soumyaranjan Pandua <soumyaranjan.pandua@involead.com>
2025-08-29 15:50:23 +0530

    modify main.py

--- main.py
+++ main.py
@@ -49,4 +49,4 @@ async def trigger_pipeline_from_uploads(
 # 👇 Add this block so Render knows how to start
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Render sets $PORT
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
